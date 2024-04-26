d68dfef765b0d54a4b5ff5b8f5a5f79113020f36
Saetch <nils.wriedt@hotmail.com>
2024-04-26 22:48:38 +0200

    added ASP.NET with Swagger for Master

--- Cluster/Master/Program.cs
+++ Cluster/Master/Program.cs
@@ -1,6 +1,51 @@
-﻿class Master{
+var builder = WebApplication.CreateBuilder(args);
 
-    public static void Main(){
-        Console.WriteLine("Ich werde der Master!");
-    }
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () => GetWeatherForecast())
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.Run();
+
+
+WeatherForecast[] GetWeatherForecast()
+{
+
+    var ret = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+
+    return ret;
+}
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+
