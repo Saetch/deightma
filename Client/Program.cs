9bb9c6956bc51abc768231f1e6007e7e10c1345c
Saetch <nils.wriedt@hotmail.com>
2024-04-26 18:40:43 +0200

    first commit

---
+++ Client/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Client{
+
+
+
+    public static void Main(){
+        Console.WriteLine("Ich werde der Client!");
+    }
+}
