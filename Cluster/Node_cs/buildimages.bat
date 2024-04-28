1a452192c0411b069c2a4021cdc95f9782ec3fc2
Saetch <nils.wriedt@hotmail.com>
2024-04-28 16:44:12 +0200

    fixed typo

--- Cluster/Node_cs/buildimages.bat
+++ Cluster/Node_cs/buildimages.bat
@@ -1,3 +1,3 @@
 docker build -f .\Dockerfiles\aot\Dockerfile --tag node_cs_aot .
 docker build -f .\Dockerfiles\aot\Dockerfile-deb --tag node_cs_aot-deb .
-docker build -d .\Dockerfiles\rt\Dockerfile --tag node_cs_rt .
+docker build -f .\Dockerfiles\rt\Dockerfile --tag node_cs_rt .
