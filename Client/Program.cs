436bddf93ad048440ef555635af4987f8fb334f2
Frederik Lehmann <frederik.martin.carl.lehmann@stud.th-luebeck.de>
2024-05-02 12:13:14 +0200

    Client added

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,8 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+BASE_URL = localhost:5000;
+
 class Client{
 
-    public static void Main(){
-        Console.WriteLine("Ich werde der Client!");
+    public static async void Main(){
+        Console.WriteLine("Please Enter your x-value");
+        string x = Console.ReadLine();
+        Console.WriteLine("Your x-value is: " + x + ". Please enter your y-value");
+        string y = Console.ReadLine();
+        Console.WriteLine("Your y-value is: " + y + ". Coordinates gathered, please wait.");
+
+        DistributedSystemWebClient webClient = new DistributedSystemWebClient(BASE_URL);
+
+        try
+        {
+            string value = await webClient.GetValue(x, y);
+            Console.WriteLine($"Value received: {value}");
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+
     }
 }
