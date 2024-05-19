a558489275666f2d6349e09089c9efa17250c93c
Saetch <nils.wriedt@hotmail.com>
2024-05-19 16:43:33 +0200

    Removed now unnecessary batch files

--- setup_everything.bat
+++
@@ -1,6 +0,0 @@
-dotnet publish .\Client\ -c Release
-docker-compose -f .\Cluster\docker-compose.yml build
-docker build -f .\Cluster\Node_cs\Dockerfiles\aot\Dockerfile --tag node_cs_aot .\Cluster\Node_cs\
-docker-compose -f .\Cluster\docker-compose.yml up -d 
-.\Cluster\launch_nodes.bat 3 2
-dotnet .\Client\bin\Release\net8.0\Client.dll
