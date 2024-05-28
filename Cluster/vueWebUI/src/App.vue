8c335a57476b5b1ac177a4cfa1788df4f49f2270
Saetch <nils.wriedt@hotmail.com>
2024-05-28 14:00:19 +0200

    [#18] add basic vue project

---
+++ Cluster/vueWebUI/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <button @click="count++">
+      Count is: {{ count }}
+    </button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const count = ref(0)
+    return { count }
+  }
+}
+</script>
